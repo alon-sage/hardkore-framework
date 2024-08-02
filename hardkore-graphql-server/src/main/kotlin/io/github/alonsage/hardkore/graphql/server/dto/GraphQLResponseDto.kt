e925da5a701d786fcd4eecbb064c69fd4e7176ac
Ivan Babintsev <alon.sage@gmail.com>
2024-08-02 09:57:04 +0400

    Fix GraphQL response extensions serialization

--- hardkore-graphql-server/src/main/kotlin/io/github/alonsage/hardkore/graphql/server/dto/GraphQLResponseDto.kt
+++ hardkore-graphql-server/src/main/kotlin/io/github/alonsage/hardkore/graphql/server/dto/GraphQLResponseDto.kt
@@ -8,11 +8,11 @@ import kotlinx.serialization.json.JsonElement
 data class GraphQLResponseDto(
     val data: JsonElement,
     val errors: List<GraphQLErrorDto>?,
-    val extensions: Map<JsonElement, JsonElement>?
+    val extensions: Map<String, JsonElement>?
 ) {
     constructor(result: ExecutionResult) : this(
         data = result.getData<JsonElement?>().toJsonElement(),
         errors = result.errors?.map(::GraphQLErrorDto)?.ifEmpty { null },
-        extensions = result.extensions?.entries?.associate { (key, value) -> key.toJsonElement() to value.toJsonElement() }
+        extensions = result.extensions?.entries?.associate { (key, value) -> key as String to value.toJsonElement() }
     )
 }

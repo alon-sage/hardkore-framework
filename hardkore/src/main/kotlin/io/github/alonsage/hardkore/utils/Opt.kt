b9e1a57c957e4518eabafd226d64f35a46ffc6c4
Ivan Babintsev <alon.sage@gmail.com>
2024-11-17 18:38:09 +0400

    FIx Opt variance and add filtering extensions

--- hardkore/src/main/kotlin/io/github/alonsage/hardkore/utils/Opt.kt
+++ hardkore/src/main/kotlin/io/github/alonsage/hardkore/utils/Opt.kt
@@ -3,11 +3,11 @@ package io.github.alonsage.hardkore.utils
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.contract
 
-sealed interface Opt<T> {
+sealed interface Opt<out T> {
     data object Missing : Opt<Any?>
 
     @JvmInline
-    value class Present<T>(val value: T) : Opt<T>
+    value class Present<out T>(val value: T) : Opt<T>
 
     companion object {
         @Suppress("UNCHECKED_CAST", "FunctionName")
@@ -46,3 +46,28 @@ inline fun <T> Opt<T>.getOrElse(block: () -> T): T =
 
 inline fun <T, R> Opt<T>.map(block: (T) -> R): Opt<R> =
     if (isMissing()) Opt.Missing() else Opt.Present(block(value))
+
+fun <T, R> Opt<T>.mapNotNull(block: (T) -> R): Opt<R & Any> =
+    if (isMissing()) {
+        Opt.Missing()
+    } else {
+        val transformed = block(value)
+        if (transformed == null) {
+            Opt.Missing()
+        } else {
+            Opt.Present(transformed)
+        }
+    }
+
+fun <T> Opt<T>.filter(block: (T) -> Boolean): Opt<T> =
+    if (isMissing() || !block(value)) Opt.Missing() else Opt.Present(value)
+
+fun <T> Opt<T>.filterNotNull(): Opt<T & Any> =
+    if (isMissing() || value == null) Opt.Missing() else Opt.Present(value)
+
+inline fun <reified T> Opt<Any?>.filterIsInstance(): Opt<T> =
+    if (isMissing() || value !is T) {
+        Opt.Missing()
+    } else {
+        Opt.Present(value as T)
+    }
